5932fce1909e36425cfae8634cc1310d17d69d8c
fernando predes <fernando_predes@hotmail.com>
2022-07-02 14:12:18 -0300

    defined Tetrominos arrays

--- tetris/app.js
+++ tetris/app.js
@@ -2,8 +2,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const grid = document.querySelector('.grid');
   let squares = Array.from(document.querySelectorAll('.grid div'));
+  const SquareDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
   const width = 10;
 
-  console.log(squares)
+  //Definindo os Tetrominoes e suas rotações
+  const lTetrominoe =[  [1, width+1, width*2+1, 2],
+                        [width, width+1, width+2, width*2+2],
+                        [1, width+1, width*2+1, width*2]
+                        [width,width*2,width*2+1,width*2+2]
+                      ];
 
+ const zTetromino = [
+                          [0,width,width+1,width*2+1],
+                          [width+1, width+2,width*2,width*2+1],
+                          [0,width,width+1,width*2+1],
+                          [width+1, width+2,width*2,width*2+1]
+                      ];
+
+ const tTetromino = [
+                          [1,width,width+1,width+2],
+                          [1,width+1,width+2,width*2+1],
+                          [width,width+1,width+2,width*2+1],
+                          [1,width,width+1,width*2+1]
+                      ];
+
+const oTetromino = [
+                          [0,1,width,width+1],
+                          [0,1,width,width+1],
+                          [0,1,width,width+1],
+                          [0,1,width,width+1]
+                      ];
+
+const iTetromino = [
+                          [1,width+1,width*2+1,width*3+1],
+                          [width,width+1,width+2,width+3],
+                          [1,width+1,width*2+1,width*3+1],
+                          [width,width+1,width+2,width+3]
+                      ];
+
+const theTrtrominoes =[lTetrominoe, zTetromino, tTetromino, oTetromino, iTetromino ];
 });
