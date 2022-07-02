06df234690a5ee3bd97fa1e5028261096c3476f1
fernando predes <fernando_predes@hotmail.com>
2022-07-02 13:45:14 -0300

    created an array with 200 divs

--- tetris/app.js
+++ tetris/app.js
@@ -1,3 +1,9 @@
-document,addEventListener('DOMContentLoaded'), () =>{
+document.addEventListener('DOMContentLoaded', () => {
 
-}
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const width = 10;
+
+  console.log(squares)
+
+});
